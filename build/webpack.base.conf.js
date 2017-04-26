a74a05c1b28a0076a96564b8120cc239d552b209
林龙辉 <540886643@qq.com>
2017-04-26 11:26:43 +0800

    * update webpack config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -54,7 +54,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('api')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
